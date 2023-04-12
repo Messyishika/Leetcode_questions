370875bf6ab3616305ca36350f4283f9dff8af32
Ishika  Garg <95583123+Messyishika@users.noreply.github.com>
2023-04-13 03:03:01 +0530

    Time: 6 ms (63.49%), Space: 10.7 MB (31.93%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string>st;
+        int n = path.size();
+        
+        for(int i = 0;i<n;i++){
+            string str;
+            if(path[i] == '/')
+                continue;
+            
+            while(i<n && path[i] != '/'){
+                str += path[i++];
+            }
+            if(str == ".."){
+                if(!st.empty())
+                    st.pop();
+                else
+                    continue;
+            }
+            else if(str == ".")
+                continue;
+            else
+                st.push(str);
+        }
+        string ans;
+        while(!st.empty()){
+            ans = "/"+st.top()+ans;
+            st.pop();
+        }
+        return ans.size() == 0 ? "/" : ans;
+    }
+};
