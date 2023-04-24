96f42f4682c2a384cb0e44b73fdb7b05bd513b37
Ishika  Garg <95583123+Messyishika@users.noreply.github.com>
2023-04-24 19:22:03 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (41.71%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& s) {
+        priority_queue<int>pq(s.begin(),s.end());
+       while(pq.size()>1){
+           int y = pq.top();
+           pq.pop();
+           int x = pq.top();
+           pq.pop();
+           
+           if(x!=y){
+               pq.push(y-x);
+           }
+       }
+        return pq.empty()?0: pq.top();
+        
+       
+    }
+};
